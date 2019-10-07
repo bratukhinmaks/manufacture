37901cf9cc0e6450e157ed39899403eccb533d96
Siarhei Drozd <drozd1586229@gmail.com>
2019-10-07 11:53:24 +0200

    added changing active link on scroll

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,8 @@ $('.nav-link').click(function () {
     $("li .nav-link").removeClass("active");
     $(this).addClass("active");
 })
+
+$(window).on('slideChange',function(event, number, element){
+    $("li .nav-link").removeClass("active");
+    document.querySelectorAll('.nav-link')[number-1].classList.toggle('active');
+});
