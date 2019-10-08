fb07faf2e9403c4e661e0668ea89fa57f8c272a3
Siarhei Drozd <drozd1586229@gmail.com>
2019-10-08 15:36:43 +0200

    added: collection slide

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,15 @@ $(window).on('slideChange',function(event, number, element){
     $("li .nav-link").removeClass("chosen");
     document.querySelectorAll('.nav-link')[number-1].classList.toggle('chosen');
 });
+
+$('.see-more-button').click(function() {
+    $('.modal-wrapper').addClass('flex');
+})
+
+$('.close-modal').click(function() {
+    $('.modal-wrapper').removeClass('flex');
+})
+
+$(window).on('slideChange',function(event, number, element){
+    $('.modal-wrapper').removeClass('flex');
+});
